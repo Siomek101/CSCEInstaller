9cc89aaca4f567a818980e66ab23e6f19e875b0f
Siomek101 <94121336+Siomek101@users.noreply.github.com>
2024-04-16 17:27:12 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace CSCEInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FilesSteamCheckbox = new System.Windows.Forms.RadioButton();
+            this.FilesManualCheckbox = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InstallDirTextBox = new System.Windows.Forms.TextBox();
+            this.LogTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.InstallSettingsPanel = new System.Windows.Forms.Panel();
+            this.FinishLabel = new System.Windows.Forms.Label();
+            this.FinishExitButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.InstallSettingsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CSCEInstaller.Properties.Resources.icon_huge;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(168, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(422, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Counter-Strike: Community Edition Installer";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.FilesManualCheckbox);
+            this.panel1.Controls.Add(this.FilesSteamCheckbox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(414, 77);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Select install option";
+            // 
+            // FilesSteamCheckbox
+            // 
+            this.FilesSteamCheckbox.AutoSize = true;
+            this.FilesSteamCheckbox.Checked = true;
+            this.FilesSteamCheckbox.Location = new System.Drawing.Point(6, 20);
+            this.FilesSteamCheckbox.Name = "FilesSteamCheckbox";
+            this.FilesSteamCheckbox.Size = new System.Drawing.Size(168, 30);
+            this.FilesSteamCheckbox.TabIndex = 1;
+            this.FilesSteamCheckbox.TabStop = true;
+            this.FilesSteamCheckbox.Text = "Copy files to Steam\r\n(show CS:CE in Steam Library)";
+            this.FilesSteamCheckbox.UseVisualStyleBackColor = true;
+            this.FilesSteamCheckbox.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // FilesManualCheckbox
+            // 
+            this.FilesManualCheckbox.AutoSize = true;
+            this.FilesManualCheckbox.Location = new System.Drawing.Point(6, 56);
+            this.FilesManualCheckbox.Name = "FilesManualCheckbox";
+            this.FilesManualCheckbox.Size = new System.Drawing.Size(124, 17);
+            this.FilesManualCheckbox.TabIndex = 2;
+            this.FilesManualCheckbox.Text = "Extract files Manually";
+            this.FilesManualCheckbox.UseVisualStyleBackColor = true;
+            this.FilesManualCheckbox.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.InstallDirTextBox);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(3, 87);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(414, 26);
+            this.panel2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Install directory:";
+            // 
+            // InstallDirTextBox
+            // 
+            this.InstallDirTextBox.Location = new System.Drawing.Point(89, 4);
+            this.InstallDirTextBox.Name = "InstallDirTextBox";
+            this.InstallDirTextBox.Size = new System.Drawing.Size(322, 20);
+            this.InstallDirTextBox.TabIndex = 1;
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Enabled = false;
+            this.LogTextBox.Location = new System.Drawing.Point(12, 257);
+            this.LogTextBox.Multiline = true;
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.Size = new System.Drawing.Size(575, 181);
+            this.LogTextBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Install";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InstallSettingsPanel
+            // 
+            this.InstallSettingsPanel.Controls.Add(this.linkLabel1);
+            this.InstallSettingsPanel.Controls.Add(this.label6);
+            this.InstallSettingsPanel.Controls.Add(this.button1);
+            this.InstallSettingsPanel.Controls.Add(this.panel1);
+            this.InstallSettingsPanel.Controls.Add(this.panel2);
+            this.InstallSettingsPanel.Location = new System.Drawing.Point(168, 40);
+            this.InstallSettingsPanel.Name = "InstallSettingsPanel";
+            this.InstallSettingsPanel.Size = new System.Drawing.Size(422, 211);
+            this.InstallSettingsPanel.TabIndex = 5;
+            // 
+            // FinishLabel
+            // 
+            this.FinishLabel.AutoSize = true;
+            this.FinishLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FinishLabel.Location = new System.Drawing.Point(172, 41);
+            this.FinishLabel.Name = "FinishLabel";
+            this.FinishLabel.Size = new System.Drawing.Size(302, 48);
+            this.FinishLabel.TabIndex = 5;
+            this.FinishLabel.Text = "Thanks for installing CS:CE!\r\nIf you want CS:CE to appear in your Steam Library,\r" +
+    "\nyou need to restart Steam.";
+            this.FinishLabel.Visible = false;
+            // 
+            // FinishExitButton
+            // 
+            this.FinishExitButton.Location = new System.Drawing.Point(511, 227);
+            this.FinishExitButton.Name = "FinishExitButton";
+            this.FinishExitButton.Size = new System.Drawing.Size(75, 23);
+            this.FinishExitButton.TabIndex = 6;
+            this.FinishExitButton.Text = "Finish";
+            this.FinishExitButton.UseVisualStyleBackColor = true;
+            this.FinishExitButton.Visible = false;
+            this.FinishExitButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Made by Siomek101";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "With help of [fill]";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(6, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(365, 30);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Note: CS:CE needs Source SDK 2013 Multiplayer to run. It\'s FREE\r\nto download on s" +
+    "team. Standalone version is coming soon.\r\n";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(6, 151);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(38, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Github";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.FinishExitButton);
+            this.Controls.Add(this.FinishLabel);
+            this.Controls.Add(this.LogTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.InstallSettingsPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Counter-Strike: Community Edition Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.InstallSettingsPanel.ResumeLayout(false);
+            this.InstallSettingsPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton FilesManualCheckbox;
+        private System.Windows.Forms.RadioButton FilesSteamCheckbox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox InstallDirTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox LogTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel InstallSettingsPanel;
+        private System.Windows.Forms.Label FinishLabel;
+        private System.Windows.Forms.Button FinishExitButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSCEInstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random Random = new Random();
+
+        object steamInstallPath = "";
+
+        private void SetInputToSteamDir()
+        {
+            InstallDirTextBox.Enabled = false;
+            if (steamInstallPath == null)
+            {
+                MessageBox.Show("Steam not found! Cannot copy files to Steam library.");
+                FilesSteamCheckbox.Checked = false;
+                FilesManualCheckbox.Checked = true;
+                InstallDirTextBox.Enabled = true;
+                return;
+            }
+            string steamPath = steamInstallPath.ToString();
+            InstallDirTextBox.Text = steamPath + @"\steamapps\sourcemods\csce\";
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // dont ask
+        }
+
+        string lastManualInput = "";
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Copy files to steam
+            if (!FilesSteamCheckbox.Checked) return;
+            lastManualInput = InstallDirTextBox.Text;
+            SetInputToSteamDir();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            // I'll do it myself
+            if (!FilesManualCheckbox.Checked) return;
+            if(lastManualInput != "") InstallDirTextBox.Text = lastManualInput;
+            InstallDirTextBox.Enabled = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            steamInstallPath = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", null);
+            SetInputToSteamDir();
+        }
+
+        private void LogBox(object msg)
+        {
+            LogTextBox.Text += msg + Environment.NewLine;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LogBox("Starting Install Process");
+            InstallSettingsPanel.Enabled = false;
+            string installPath = InstallDirTextBox.Text;
+            if(!Directory.Exists(installPath))
+            {
+                LogBox("Directory " + installPath + " doesn't exist. Creating...");
+                Directory.CreateDirectory(installPath);
+            } else
+            {
+                if(MessageBox.Show("Directory already exists. Do you want to replace it?", "Question", MessageBoxButtons.YesNo) != DialogResult.Yes)
+                {
+                    InstallSettingsPanel.Enabled = true;
+                    LogBox("Installation aborted.");
+                    return;
+                }
+            }
+            // Old approach
+            //LogBox("Extracting files into temporary folder...");
+            //var tempFolder = Path.GetTempPath() + @"CSCE_TEMP" + Random.Next(100000, (int) (int.MaxValue/100));
+            //Directory.CreateDirectory(tempFolder);
+            //LogBox("Temporary folder: " + tempFolder);
+
+            LogBox("Extracting files using 7-zip...");
+            var sevenzipArgs = "x archive.7z -o"+installPath;
+            Process.Start("7z.exe", sevenzipArgs).WaitForExit();
+
+            LogBox("Done!");
+            InstallSettingsPanel.Visible = false;
+            FinishLabel.Visible = true;
+            FinishExitButton.Visible = true;
+
+            //LogBox("Removing temporary folder...");
+            //Directory.Delete(tempFolder, true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
